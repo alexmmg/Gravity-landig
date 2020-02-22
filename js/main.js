a4fe37276d089995daa9d2dc15497236feb919c3
alexmmg <mmg.prokazyuk.o@gmail.com>
2020-02-22 23:37:13 +0200

    section 3 done (1:48:15)

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ $(function () {
         loop: true,
         margin: 30,
         nav: true,
+        startPosition: 1,
+        navText: ["<img alt='Left' src='img/left.svg'>", "<img alt='Right' src='img/right.svg'>"],
+        dots: false,
         responsive: {
             0: {
                 items: 1
@@ -12,7 +15,7 @@ $(function () {
                 items: 1
             },
             1000: {
-                items: 2
+                items: 1
             }
         }
     });
