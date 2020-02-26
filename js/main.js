d697839a26c8f8d7ba8d3a7cebc6aa69673173ea
alexmmg <mmg.prokazyuk.o@gmail.com>
2020-02-26 21:36:14 +0200

    footer done (3:01:00)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('.owl-carousel').owlCarousel({
+    $('.works-carousel').owlCarousel({
         // stagePadding: 50,
         loop: true,
         margin: 30,
@@ -19,5 +19,27 @@ $(function () {
             }
         }
     });
+
+    $('.contributor-carousel').owlCarousel({
+        // stagePadding: 50,
+        loop: true,
+        margin: 0,
+        nav: true,
+        // startPosition: 1,
+        navText: ["<img alt='Left' src='img/left.svg'>", "<img alt='Right' src='img/right.svg'>"],
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
 });
 
